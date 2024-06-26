210c0758049796f99e8136cbeae8da0f61c5ff50
MatiasPhoenix <matias.la19@gmail.com>
2024-06-26 09:34:31 +0200

    update angular e basi progetto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { LateralBarComponent } from './Components/lateral-bar/lateral-bar.component';
+import { PromemoriaComponent } from './Components/promemoria/promemoria.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:HomeComponent},
+  { path: 'lateral-bar', component:LateralBarComponent},
+  { path: 'promemoria', component:PromemoriaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
